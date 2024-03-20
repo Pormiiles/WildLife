ea575ce308c32d322ebd4adf56e23d30c32727b5
wsprego <wesleysilva.santos076@gmail.com>
2024-03-20 15:55:47 -0300

    add botao swicht e css do mesmo

--- script.js
+++ script.js
@@ -34,3 +34,13 @@ function prevSlider() {
 
 btnNext.addEventListener('click', nextSlider)
 btnPrev.addEventListener('click', prevSlider)
+
+// codigo do botao swicht
+document.getElementById("mySwitch").addEventListener("change", function() {
+    if(this.checked) {
+      window.location.href = "pag2.html"; // Altere "pagina1.html" para o caminho da sua página destino
+    } else {
+      window.location.href = "index.html"; // Altere "pagina2.html" para o caminho da sua outra página destino
+    }
+  });
+  
